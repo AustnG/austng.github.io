bd91efe72f713fa4d12cb02aa436354272c79f4e
Austin <austin.greer6@gmail.com>
2020-11-19 15:22:19 -0600

    Add files via upload

---
+++ header.js
@@ -0,0 +1,25 @@
+document.write(`
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+    <!-- displays site properly based on user's device -->
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Bai+Jamjuree:400,600&display=swap" rel="stylesheet">
+    
+  </head>
+  
+  <body>
+    <!-- Navigation -->
+    <div class="button-box" style="width:100%; height:100px; background-color:black; color:white; text-align:center;">
+        <a href="http://austng.github.io"><button class="ios-button">Home</button></a>
+        <a href="fitness/index2.html"><button class="mac-button">Page 2</button></a>
+        <a href=""><button class="mac-button">LINK 3</button></a>
+    </div>
+  </body>
+</html>
+
+`);
